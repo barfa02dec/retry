ad3a844b2a20c5410220744151d148ced6399efd
barfa02dec <51827450+barfa02dec@users.noreply.github.com>
2021-02-23 13:29:11 +0530

    Update UserServiceImp.java

--- src/main/java/com/hm/external/service/UserServiceImp.java
+++ src/main/java/com/hm/external/service/UserServiceImp.java
@@ -18,40 +18,6 @@ import com.hm.external.model.User;
 public class UserServiceImp implements UserServiceInf{
 	private static int counter = 1;
 	private static int count = 0;
-	
-//	@Autowired
-//	UserRepository userRepository;
-//	@Autowired
-//	ResilienceConfig resilienceConfig;
-//	
-//	@Override
-//	@Retry(name = "retryUser",fallbackMethod = "test")
-//	//@Retryable(value = {InternalServerError.class,ArithmeticException.class,HttpClientErrorException.class},maxAttempts = 3 )
-//	public User getUserById(int userId) {
-//		RestTemplate restTemplate = new RestTemplate();
-//		ResponseEntity<User> user = restTemplate.exchange(new RequestEntity<>(userId,HttpMethod.GET,URI.create("http://localhost:8081/user/"+userId)),User.class);	
-////		throw new ArithmeticException("");
-//		return user.getBody();
-//	}
-//	@Override
-//	public User addUser(Integer userId) throws Exception {
-////		TestFallback fallback = new TestFallback();
-////		return userRepository.save(fallback.getUserById(userId));
-////		throw new ArithmeticException("");
-//		return userRepository.save(getUserById(userId));
-//	}
-//		
-//	public User test(int userId,HttpClientErrorException t) throws Exception{
-//		System.out.println(1);
-//		return null;
-//	}
-//	
-//	@Recover
-//	public User recover(ArithmeticException t){
-//	    System.out.println("TEst");
-//		return new User();
-//	}
-
 	@Override
 	public User getUser() {
 		System.out.println("Retry : "+ counter++);
@@ -74,7 +40,6 @@ public class UserServiceImp implements UserServiceInf{
 	@Override
 	public User addUserFallBack(RuntimeException e) {
 		System.out.println("Max attempt faild. Fall back method called");
-		throw new HttpServerErrorException(HttpStatus.TOO_MANY_REQUESTS);
+		return null;
 	}
-	
 }
